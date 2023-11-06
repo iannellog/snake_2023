cba7770e532830686be5cb59c4f35524a37d0817
Giulio Iannello <g.iannello@unicampus.it>
2023-11-06 15:43:17 +0100

    sketch of the main program

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Created on Sept. 29, 2023
+by Giulio Iannello
+"""
+
+# Snake game
+
+# get layout and moves
+# create snake object (including an empty initial path)
+# create play field object and put snake in start position
+# for m in moves:
+#    check if move is valid for play field and if snake finds food
+#    if move is valid:
+#        check if move is valid for the snake, taking into account if it eats food
+#        if move is valid:
+#            update play field
+#            update snake (including its path)
+#    if move is not valid:
+#        break
+# display play field and snake (including its path)
+
