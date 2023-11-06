b2585735204fb7d808f2d313da1dcd428fd6fa6f
Giulio Iannello <g.iannello@unicampus.it>
2023-11-06 18:00:43 +0100

    Implementato il metodo check_move della classe Snake Implementati i test realativi

--- snake.py
+++ snake.py
@@ -7,10 +7,19 @@ by Giulio Iannello
 class Snake:
     """
     Implements a moving snake including its path
+    snake representation
+    - body: list of coordinates, the head of the list represents the position
+            of the snake head
+    - tail: list of coordinates, the head of the list represents the initial
+            position of the snake head, the tail of the list represents the
+            last position left by the snake
+
+    coordinates are represented by a list [row, column]
     """
 
     def __init__(self):
-        pass
+        self.body = []
+        self.tail = []
 
     def place_in_start_position(self, initial_position):
         """
@@ -18,14 +27,14 @@ class Snake:
         :param initial_position: initial position
         :return: None
         """
-        pass
+        self.body = [initial_position]
 
     def get_head_position(self):
         """
         return the snake head position
         :return: snake head position
         """
-        return [0, 0]
+        return self.body[0]
 
     def check_move(self, new_position, eats):
         """
@@ -34,6 +43,38 @@ class Snake:
         :param eats: if the new position contains food
         :return: if the move is valid
         """
+        body_copy = self.body.copy()
+        if not eats:  # tail snake shifts by one position
+            body_copy.pop(len(body_copy)-1)
+        if new_position in body_copy:  # snake head overlap with its body
+            return False
+        # assert: snake head does not overlap with its body
+
+        # check if snake cross its body
+        # strategy: check if snake head (h) has adjacent body segments (b) all around itself
+        #
+        #   b b       b b      h b      b h
+        #    X   or    X   or   X   or   X
+        #   b h       h b      b b      b b
+        direction = [(new_position[0]-body_copy[0][0]), (new_position[1]-body_copy[0][1])]
+        if direction == [1, 1] and \
+                [(new_position[0]-1), new_position[1]] in body_copy and \
+                [(new_position[0]), (new_position[1]-1)] in body_copy:  # move SE
+            return False
+        elif direction == [1, -1] and \
+                [(new_position[0]-1), new_position[1]] in body_copy and \
+                [(new_position[0]), (new_position[1]+1)] in body_copy:  # move SW
+            return False
+        elif direction == [-1, -1] and \
+                [(new_position[0]+1), new_position[1]] in body_copy and \
+                [(new_position[0]), (new_position[1]+1)] in body_copy:  # move NW
+            return False
+        elif direction == [-1, 1] and \
+                [(new_position[0]+1), new_position[1]] in body_copy and \
+                [(new_position[0]), (new_position[1]-1)] in body_copy:  # move NE
+            return False
+        else:  # move S, W, N, E are valid since snake head does not overlap with its body
+            pass
         return True
 
     def update(self, new_position, eats):
--- snake_test.py
+++ snake_test.py
@@ -15,10 +15,35 @@ class Snake_test(unittest.TestCase):
         head_position = snake.get_head_position()
         self.assertEqual(head_position, position, f'wrong head position ({head_position} instead of {position})')
 
-    def test_check_move_nofood_valid(self):
+    def test_check_move_nofood_nooverlap(self):
         snake = Snake()
-        snake.place_in_start_position([0, 0])
+        snake.body = [[1, 0], [1, 1], [0, 1], [0, 0]]
+        self.assertTrue(snake.check_move([2, 0], False))
+
+    def test_check_move_nofood_overlap(self):
+        snake = Snake()
+        snake.body = [[1, 0], [1, 1], [0, 1], [0, 0]]
+        self.assertFalse(snake.check_move([0, 1], False))
+
+    def test_check_move_food_nooverlap(self):
+        snake = Snake()
+        snake.body = [[1, 0], [1, 1], [0, 1], [0, 0]]
+        self.assertTrue(snake.check_move([2, 0], True))
+
+    def test_check_move_food_overlap(self):
+        snake = Snake()
+        snake.body = [[1, 0], [1, 1], [0, 1], [0, 0]]
+        self.assertFalse(snake.check_move([0, 1], True))
+
+    def test_check_move_nofood_nocross(self):
+        snake = Snake()
+        snake.body = [[1, 0], [1, 1], [0, 0]]
         self.assertTrue(snake.check_move([0, 1], False))
+
+    def test_check_move_food_cross(self):
+        snake = Snake()
+        snake.body = [[1, 0], [1, 1], [0, 0]]
+        self.assertFalse(snake.check_move([0, 1], True))
 
     def test_update(self):
         snake = Snake()
