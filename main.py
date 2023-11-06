9309084b729cb5d4276a5f01c2ba3d1281632b8d
Giulio Iannello <g.iannello@unicampus.it>
2023-11-06 16:41:22 +0100

    Completato il main

--- main.py
+++ main.py
@@ -18,19 +18,29 @@ with open(game_file, 'r') as file:
 with open(game_info["field_in"], 'r') as file:
     field_info = load(file)
     file.close()
+
 # create snake object (including an empty initial tail)
 snake = Snake()
 # create play field object
 play_field = Play_field(field_info["rows"], field_info["cols"], field_info["blocks"], field_info["food"])
 # put snake in start position
-# for m in moves:
-#    check if move is valid for play filed; if it is, return the new head position and if it contains food
-#    if move is valid:
-#        check if new position is valid for the snake, taking into account if it eats food
-#        if new position is valid:
-#            update play field
-#            update snake (including its tail)
-#    if move is not valid:
-#        break
+snake.place_in_start_position(game_info["start"])
+
+# start the game
+for m in game_info["moves"].split(' '):
+    #    check if move is valid for play filed; if it is, return the new head position and if it contains food
+    valid_move, new_head_position, food = play_field.check_move(snake.get_head_position(), m)
+    if valid_move:
+        # check if new position is valid for the snake, taking into account if it eats food
+        valid_move = snake.check_move(new_head_position, food)
+        if valid_move:
+            # update play field
+            play_field.update(new_head_position)
+            # update snake (including its tail)
+            snake.update(new_head_position, food)
+    if not valid_move:
+        break
+
 # display play field and snake (including its path)
+print('End')
 
