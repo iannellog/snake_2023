6869fab671749deebc98c344480b6a3da823c36a
Giulio Iannello <g.iannello@unicampus.it>
2023-11-06 15:47:19 +0100

    added the Play_field class

---
+++ play_field.py
@@ -0,0 +1,36 @@
+"""
+Created on Sept. 30, 2023
+by Giulio Iannello
+"""
+
+
+class Play_field:
+    """
+    Implements a play field
+    """
+
+    def __init__(self):
+        pass
+
+    def place_snake(self, initial_position):
+        """
+        store the initial position of the head of the snake
+        :param initial_position: initial position of the head of the snake
+        :return: None
+        """
+        pass
+
+    def check_move(self, move):
+        """
+        check if move is valid for play field and if snake finds food
+        :param move: move to be taken by the snake
+        :return: if the snake has found food
+        """
+        return False
+
+    def update(self, move):
+        """
+        update play field
+        :param move: move to be taken by the snake
+        :return: None
+        """
